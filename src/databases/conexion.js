cb03cc36d7a8b65b4d2fc808b255fa53eb6fb309
valen1079534 <valendiazlerma16@gmail.com>
2024-06-12 09:59:36 -0500

    Backend

--- src/databases/conexion.js
+++ src/databases/conexion.js
@@ -1,31 +1,22 @@
 import { createPool } from "mysql2/promise";
-import dotenv from "dotenv";
-import { fileURLToPath } from "url";
-import path from "path";
-
-// Obtener la ruta del archivo actual 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
-dotenv.config({ path: path.resolve(__dirname, "../../src/env/.env") });
-
-
-
-
+/* import dotenv from "dotenv";
+ */
+/* dotenv.config({ path: "./src/env/.env" }); */
 
 export const pool = createPool({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    port: process.env.DB_PORT,
-    database: process.env.DB_DATABSE
-})
+  host: 'monorail.proxy.rlwy.net',
+  user: 'root',
+  password: 'PvpFKCwRAgmvTwJTjvziuTuXnSlqgNqb',
+  port: 30386,
+  database: 'railway',
+});
 
-//Validar conexión a la base de datos
-pool.getConnection().then(connect => {
-    console.log("Conexión a base de datos exitosa.");
+pool
+  .getConnection()
+  .then((connect) => {
+    console.log("Conexión a la database coffeeoffer exitosa");
     connect.release();
-})
-    .catch(error => {
-        console.error("Conexion a base de datos fallida. " + error);
-})  
+  })
+  .catch((error) => {
+    console.log("Conexión a la database fallida " + error);
+  });
