57950464466c70302fa87f599a615c162bcfca0d
valen1079534 <valendiazlerma16@gmail.com>
2024-06-12 09:45:23 -0500

    Backend

--- src/database/conexion.js
+++ src/database/conexion.js
@@ -1,13 +1,13 @@
 import { createPool } from "mysql2/promise";
-import dotenv from "dotenv";
-
-dotenv.config({ path: "./src/env/.env" });
+/* import dotenv from "dotenv";
+ */
+/* dotenv.config({ path: "./src/env/.env" }); */
 
 export const pool = createPool({
   host: 'monorail.proxy.rlwy.net',
   user: 'root',
   password: 'PvpFKCwRAgmvTwJTjvziuTuXnSlqgNqb',
-  port: '30386',
+  port: 30386,
   database: 'railway',
 });
 
